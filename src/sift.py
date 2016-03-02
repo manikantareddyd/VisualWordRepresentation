da851a01e6d186f5cfd4e17fd638806f71f499ad
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-03-02 17:36:24 +0530

    Memory Optimizations - A pickled file is uncommited

--- src/sift.py
+++ src/sift.py
@@ -3,9 +3,9 @@ from os import listdir
 import numpy as np
 class SIFT:
 
-    def __init__(self):
+    def __init__(self,n):
         self.FeatureVectors = []
-        self.imgfiles   = listdir('data')
+        self.imgfiles   = sorted(listdir('../ukbench/full'))[:n]
         self.imageFeat = {}
         self.imageName  = []
         self.extractAll()
@@ -14,12 +14,17 @@ class SIFT:
             self.extract(imgfile)
         return
     def extract(self,imgfile):
-        sift = cv2.xfeatures2d.SIFT_create()
-        img = cv2.imread('data/'+imgfile)
+        sift = cv2.xfeatures2d.SIFT_create(nfeatures=500)
+        img = cv2.imread('../ukbench/full/'+imgfile)
         gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         kp,des = sift.detectAndCompute(gray,None)
         self.imageFeat[str(imgfile)] = np.array(des)
         for j in des:
             self.FeatureVectors.append(j)
             self.imageName.append(imgfile)
+        del sift
+        del img
+        del gray
+        del des
+        del kp
         return
